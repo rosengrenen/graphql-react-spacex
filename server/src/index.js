b229509a3e80db7eb61624e4598e0baf9584c692
Rasmus Rosengren <rsrp98@gmail.com>
2019-03-10 16:59:16 +0100

    Connected graphql to spaceX api

--- server/src/index.js
+++ server/src/index.js
@@ -3,20 +3,43 @@ const {
   ApolloServer,
   gql
 } = require('apollo-server-express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql `
+  type Launch {
+    flight_number: ID
+    mission_name: String,
+    launch_year: String,
+    launch_date_local: String,
+    launch_success: Boolean,
+    rocket: Rocket
+  }
+
+  type Rocket {
+    rocket_id: String,
+    rocket_name: String,
+    rocket_type: String
+  }
+
   type Query {
-    hello(name: String): String
+    launches: [Launch],
+    launch(flight_number: ID): Launch,
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (_, {
-      name
-    }) => `Hello ${name || 'World'}`
+    launches: (_, {}) => {
+      return axios.get('https://api.spacexdata.com/v3/launches').then(result => result.data);
+    },
+    launch: (_, {
+      flight_number
+    }) => {
+      return axios.get(`https://api.spacexdata.com/v3/launches/${flight_number}`).then(result => result.data);
+    }
   },
 };
 
@@ -26,6 +49,11 @@ const server = new ApolloServer({
 });
 
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 server.applyMiddleware({
   app,
   path: '/graphql'
@@ -34,5 +62,5 @@ server.applyMiddleware({
 app.listen({
     port: 4000
   }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
 );
